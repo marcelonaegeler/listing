9c296ff10b0598eec6ef4028f50240a79edaad1f
Marcelo A Naegeler <marcelo.andre.naegeler@gmail.com>
2016-04-08 15:42:04 -0300

    Adicionado arquivo para lista

---
+++ index.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>List</title>
+
+	<link href="https://fonts.googleapis.com/css?family=Noto+Sans:400,700" rel="stylesheet" type="text/css">
+	<link rel="stylesheet" href="stylesheets/style.css">
+	<link rel="stylesheet" href="stylesheets/dropdown.css">
+	<link rel="stylesheet" href="stylesheets/grid.css">
+
+</head>
+<body>
+	<?php include '_header.html' ?>
+
+	<section class="content">
+
+		<section class="topbar card">
+			<h1 class="heading-1">Title test</h1>
+		</section>
+
+		<div class="row">
+		
+			<div class="sidebar card">
+				<ul>
+
+					<li>
+						<div class="">
+							<a href="#" class="sidebar-item">Categoria 01</a>
+						</div>
+						<div class=""></div>
+					</li>
+
+					<li>
+						<div class="">
+							<a href="#" class="sidebar-item sidebar-item--active">Categoria 02</a>
+						</div>
+						<div class=""></div>
+					</li>
+
+				</ul>
+			</div>
+
+			<div class="card list-container">
+				
+				<div class="list-container-item">
+					<strong>Jericlérison Souza</strong>
+				</div>
+
+				<div class="list-container-item">
+					<strong>Cleiton Karllos</strong>
+				</div>
+
+			</div>
+
+		</div>
+
+	</section>
+	
+</body>
+</html>
---
+++ list.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>List</title>
+
+	<link href="https://fonts.googleapis.com/css?family=Noto+Sans:400,700" rel="stylesheet" type="text/css">
+	<link rel="stylesheet" href="stylesheets/style.css">
+	<link rel="stylesheet" href="stylesheets/dropdown.css">
+	<link rel="stylesheet" href="stylesheets/grid.css">
+
+</head>
+<body>
+	<?php include '_header.html' ?>
+
+	<section class="content content-mid">
+
+		<section class="topbar card">
+			<h1 class="heading-1">Listing</h1>
+		</section>
+
+		<section class="list-by-date card">
+
+			<div class="filters">
+				mostrando <span class="highlight">todas as categorias</span>
+			</div>
+			
+			<div class="period-item">
+				<div class="period-date">08/04</div>
+
+				<div class="period-list-item row">
+					<div class="column column-4">
+						<span class="circular-marker bg-red"></span> 
+						<a href="#" class="period-list-item-name">List item 01</a>
+					</div>
+					<div class="column column-3">categoria 01</div>
+					<div class="column column-3">R$50,00 (lucro R$15,00)</div>
+					<div class="column column-2">Editar</div>
+				</div>
+
+				<div class="period-list-item row">
+					<div class="column column-4">
+						<span class="circular-marker bg-red"></span> 
+						<a href="#" class="period-list-item-name">List item 02</a>
+					</div>
+					<div class="column column-3">categoria 03</div>
+					<div class="column column-3">R$60,00 (lucro R$20,00)</div>
+					<div class="column column-2">Editar</div>
+				</div>
+
+			</div>
+
+			<div class="period-item">
+				<div class="period-date">10/04</div>
+
+				<div class="period-list-item row">
+					<div class="column column-4">
+						<span class="circular-marker bg-red"></span> 
+						<a href="#" class="period-list-item-name">List item 03</a>
+					</div>
+					<div class="column column-3">categoria 01</div>
+					<div class="column column-3">R$50,00 (lucro R$15,00)</div>
+					<div class="column column-2">Editar</div>
+				</div>
+
+				<div class="period-list-item row">
+					<div class="column column-4">
+						<span class="circular-marker bg-red"></span> 
+						<a href="#" class="period-list-item-name">List item 04</a>
+					</div>
+					<div class="column column-3">categoria 03</div>
+					<div class="column column-3">R$60,00 (lucro R$20,00)</div>
+					<div class="column column-2">Editar</div>
+				</div>
+
+			</div>
+
+		</section>
+
+	</section>
+	
+</body>
+</html>
